662bdf1c0eab5fff0f3c5317f7e56f9e3874adb0
christopher_bonnett <c.bonnett@gmail.com>
2014-08-10 01:34:12 +0200

    starting better documentation

--- src/SkyNet.py
+++ src/SkyNet.py
@@ -1,8 +1,14 @@
 """
-This is a (poor man's) wrapper for SkyNet neural network sofware.
-You need to install SkyNet :
-SkyNet is an efficient and robust neural network
-training tool for machine learning.
+This is a python wrapper for SkyNet neural network sofware.
+
+A python wrapper for SkyNet neural network that can de downloaded here : http://ccpforge.cse.rl.ac.uk/gf/project/skynet/
+
+SkyNet is an efficient and robust neural network training code for machine learning. It is able to train large and deep feed-forward neural networks, including autoencoders, for use in a wide range of supervised and unsupervised learning applications, such as regression, classification, density estimation, clustering and dimensionality reduction. SkyNet is implemented in C/C++ and fully parallelised using MPI.
+
+SKyNet is written by Philip Graff, Farhan Feroz, Michael P. Hobson, Anthony N. Lasenby
+
+reference : http://xxx.lanl.gov/abs/1309.0790
+
 """
 
 # Authors: CHRISTOPHER BONNETT <c.bonnett@gmail.com>
