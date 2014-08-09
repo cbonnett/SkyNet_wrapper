1c5b39f9fc8bdd97dfeb82e1dba54e9556f744ce
christopher_bonnett <c.bonnett@gmail.com>
2014-08-09 17:30:17 +0200

    update docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ mpl.use("Agg")
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('_themes'))
 
 # -- General configuration ------------------------------------------------
 
