6cb35d2e6cf39cf9a02d48c5b8b9ac22882a9f6d
christopher_bonnett <c.bonnett@gmail.com>
2014-08-10 21:39:07 +0200

    path edit

--- src/SkyNet.py
+++ src/SkyNet.py
@@ -1,9 +1,19 @@
 """
-A python wrapper for SkyNet neural network that can de downloaded here : http://ccpforge.cse.rl.ac.uk/gf/project/skynet/
+A python wrapper for SkyNet neural network that
+can de downloaded here :
+http://ccpforge.cse.rl.ac.uk/gf/project/skynet/
 
-SkyNet is an efficient and robust neural network training code for machine learning. It is able to train large and deep feed-forward neural networks, including autoencoders, for use in a wide range of supervised and unsupervised learning applications, such as regression, classification, density estimation, clustering and dimensionality reduction. SkyNet is implemented in C/C++ and fully parallelised using MPI.
+SkyNet is an efficient and robust neural network
+training code for machine learning. It is able to
+train large and deep feed-forward neural networks, 
+including autoencoders, for use in a wide range of 
+supervised and unsupervised learning applications,
+such as regression, classification, density estimation,
+clustering and dimensionality reduction. SkyNet is
+implemented in C/C++ and fully parallelised using MPI.
 
-SkyNet is written by Philip Graff, Farhan Feroz, Michael P. Hobson, Anthony N. Lasenby
+SkyNet is written by Philip Graff, Farhan Feroz, 
+Michael P. Hobson, Anthony N. Lasenby
 
 reference : http://xxx.lanl.gov/abs/1309.0790
 
