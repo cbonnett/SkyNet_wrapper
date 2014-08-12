604778710629dc3805621bdf8e79f22b91ccbe13
christopher_bonnett <c.bonnett@gmail.com>
2014-08-12 08:10:55 +0200

    retrun df with errors form SkyNet stdout

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot','seaborn']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
