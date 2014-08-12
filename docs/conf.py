d95cf192015b96f53ad9b6a3f762cb470107af82
christopher_bonnett <c.bonnett@gmail.com>
2014-08-12 08:08:54 +0200

    docs debugging

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 # -*- coding: utf-8 -*-
 #
