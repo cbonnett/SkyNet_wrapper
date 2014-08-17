03fb0ce3c43176e4561aff79914b0ce2b980214b
christopher_bonnett <c.bonnett@gmail.com>
2014-08-17 13:15:03 +0200

    more informative install.py

--- src/install.py
+++ src/install.py
@@ -9,8 +9,22 @@ except:
     print "Exiting the setup"
     sys.exit(1)
 
-os.mkdir(''.join([SkyNet_path, 'config_files']))
-os.mkdir(''.join([SkyNet_path, 'network']))
-os.mkdir(''.join([SkyNet_path, 'predicitions']))
-os.mkdir(''.join([SkyNet_path, 'train_valid']))
+try:
+    os.mkdir(''.join([SkyNet_path, 'config_files']))
+    if os.path.isdir(''.join([SkyNet_path, 'config_files'])):
+            print 'Created {}'.format(''.join([SkyNet_path, 'config_files']))
+    os.mkdir(''.join([SkyNet_path, 'network']))
+    if os.path.isdir(''.join([SkyNet_path, 'network'])):
+        print 'Created {}'.format(''.join([SkyNet_path, 'network']))
+    os.mkdir(''.join([SkyNet_path, 'predictions']))
+    if os.path.isdir(''.join([SkyNet_path, 'predictions'])):
+        print 'Created {}'.format(''.join([SkyNet_path, 'predictions']))
+    os.mkdir(''.join([SkyNet_path, 'train_valid']))
+    if os.path.isdir(''.join([SkyNet_path, 'train_valid'])):
+        print 'Created {}'.format(''.join([SkyNet_path, 'train_valid']))
+except:
+    print "Unable to write path"
+    print "Check your write permissions"
+    print "Exiting the setup"
+    sys.exit(1)
 
